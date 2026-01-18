380ae82c265baac16e8f1bea4e0494aeb722ff6a
Nikita <nikichx2@gmail.com>
2026-01-18 12:00:15 +0100

    Initial commit

---
+++ src/main/kotlin/com/nikichxp/busconnector/BusconnectorApplication.kt
@@ -0,0 +1,11 @@
+package com.nikichxp.busconnector
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BusconnectorApplication
+
+fun main(args: Array<String>) {
+	runApplication<BusconnectorApplication>(*args)
+}
---
+++ src/test/kotlin/com/nikichxp/busconnector/BusconnectorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nikichxp.busconnector
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BusconnectorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
